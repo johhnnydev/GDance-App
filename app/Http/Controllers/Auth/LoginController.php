52c64d9697e7b8cb757888c4670d7dbf23394f18
maturepizza <30493210+maturepizza@users.noreply.github.com>
2018-03-25 19:31:49 +0800

    added override login method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,7 +19,46 @@ class LoginController extends Controller
     */
 
     use AuthenticatesUsers;
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
 
+        // find the user with this email
+        $user = DB::table('users')->where('email', $request->email)->first();
+
+        // if($user->status == 'active'){
+        //     return 'the user is active log him/her in';
+        // }
+        
+        if($user->status == 'disabled'){
+            return back()->with('message', 'Your account is disabled. Contact your administrator for further details.');
+        }
+        
+        // get the status of the account (disabled/active)
+        // if the status is disabled redirectback
+        // with account is disabled message
+        // else continue with the process
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
     /**
      * Where to redirect users after login.
      *
