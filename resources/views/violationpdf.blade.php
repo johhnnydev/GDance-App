2aa43edd98fb31e9e45ed2c13ec131364ee11fe8
maturepizza <30493210+maturepizza@users.noreply.github.com>
2018-03-25 15:16:22 +0800

    added count on violation pdf

--- resources/views/violationpdf.blade.php
+++ resources/views/violationpdf.blade.php
@@ -13,6 +13,7 @@
 			border: 1px solid black;
 			table-layout: fixed;
 			width: 100%; 
+			margin-bottom: 10px;
 		}
 		th,td {
 			border: 1px solid black;
@@ -42,5 +43,7 @@
 			</tr>
 		@endforeach
 	</table>
+	
+	<em>{{ count($violations) }} violation record found</em>
 </body>
 </html>
